9008a4349064da17417f1d88d3575f79d78d1494
nishantjangid <nishantsharma39262@gmail.com>
2023-09-16 17:40:35 +0530

    Nishant V1.0.1

--- controllers/users/userDeposite.js
+++ controllers/users/userDeposite.js
@@ -3,6 +3,16 @@ const { json } = require("body-parser");
 //all deposite comes here
 exports.allDeposite = (req, res) => {};
 
+// user deposit request
+exports.requestDeposit = (req,res,next) => {
+    try{
+        console.log(req);
+    }catch(error){
+        console.log(error);
+        next(error)
+    }
+}
+
 //approve deposite comes here
 exports.approveDeposite = (req, res) => {};
 
