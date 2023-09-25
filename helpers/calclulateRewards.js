25d2887a0d4c03347c1abd96690996efa47da060
ThinkerTechnology <defichainnfts@gmx.de>
2023-09-25 13:09:16 +0530

    sourabh 1.7.0

--- helpers/calclulateRewards.js
+++ helpers/calclulateRewards.js
@@ -4,11 +4,6 @@ const withdraw = require("../models/withdrawal");
 const levelIncome = require("../models/levelIncome");
 const calclulateRewadsPerDay = async (userId) => {
   let userInfo = await investment.findOne({ userId });
-<<<<<<< HEAD
-
-=======
-  if(!userInfo) return 0;
->>>>>>> f42f54f44d83f05ae41676fb8e147820ef97dbd8
   let timedifference =
     Math.floor(Date.now() / 1000) -
     Math.floor(Date.parse(userInfo.createdAt) / 1000);
@@ -19,59 +14,15 @@ const calclulateRewadsPerDay = async (userId) => {
 
 const calclulateRewads = async (userId) => {
   let userInfo = await investment.findOne({ userId });
-  if(!userInfo) return 0;
+  if (!userInfo) return 0;
   let timedifference =
     Math.floor(Date.now() / 1000) -
     Math.floor(Date.parse(userInfo.createdAt) / 1000);
   let totalRewards = userInfo.amount * (0.08 / 86400) * timedifference;
   totalRewards += userInfo.rewards;
-  return totalRewards;
+  if (totalRewards > 2 * userInfo.amount) return 2 * userInfo.amount;
+  else totalRewards;
 };
-
-// const CalclulateLevelIncome = async (userId) => {
-//   let userInfo = await User.findOne({ userId });
-//   if (userInfo.isInvested) {
-//     let rewardPerceantege = [5, 2, 1];
-//     let member, member2, member3;
-//     let memberInfo, memberInfo2, memberInfo3;
-//     let memberInvestedAmount, memberInvestedAmount2, memberInvestedAmount3;
-//     let totalRewards = 0;
-//     // Level 1
-//     for (let i = 0; i < userInfo.refferedTo.length; i++) {
-//       member = userInfo.refferedTo[i];
-//       memberInfo = await User.findOne({ userId: member });
-//       if (memberInfo.isInvested) {
-//         memberInvestedAmount = await investment.findOne({ userId: member });
-//         memberInvestedAmount = memberInvestedAmount.amount;
-//         totalRewards = (memberInvestedAmount * rewardPerceantege[0]) / 100;
-//         for (let j = 0; j < memberInfo.refferedTo.length; j++) {
-//           member2 = memberInfo.refferedTo[j];
-//           memberInfo2 = await User.findOne({ userId: member2 });
-//           if (memberInfo2.isInvested) {
-//             memberInvestedAmount2 = await investment.findOne({
-//               userId: member2,
-//             });
-//             memberInvestedAmount2 = memberInvestedAmount2.amount;
-//             totalRewards = (memberInvestedAmount2 * rewardPerceantege[1]) / 100;
-//             for (let k = 0; k < memberInfo2.refferedTo.length; k++) {
-//               member3 = memberInfo2.refferedTo[k];
-//               memberInfo3 = await User.findOne({ userId: member3 });
-//               if (memberInfo3.isInvested) {
-//                 memberInvestedAmount3 = await investment.findOne({
-//                   userId: member3,
-//                 });
-//                 memberInvestedAmount3 = memberInvestedAmount3.amount;
-//                 totalRewards =
-//                   (memberInvestedAmount3 * rewardPerceantege[2]) / 100;
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//     return totalRewards;
-//   } else return 0;
-// };
 
 const CalclulateLevelIncome = async (userId) => {
   let totalRewards = await levelIncome.aggregate([
