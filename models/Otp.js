8e87b12bc890da4fd3a77d6d29f3bfabcb281574
nishantjangid <nishantsharma39262@gmail.com>
2023-09-29 13:35:08 +0530

    Nishant otp model

--- models/Otp.js
+++ models/Otp.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const OTPs = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    email: { type: String, required: true },
     otp:{type:Number,require:true}
   },
   { timestamps: true }
