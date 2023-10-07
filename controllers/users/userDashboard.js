b6960d7e87cb6e9eff7a95088c3da11b01839c4b
ThinkerTechnology <defichainnfts@gmx.de>
2023-10-07 12:24:45 +0530

    sourabh abcde

--- controllers/users/userDashboard.js
+++ controllers/users/userDashboard.js
@@ -9,6 +9,8 @@ const {
 } = require("../../helpers/calclulateRewards");
 const calclulateRewardsForBigLag = require("../../helpers/calclulateBiglagRewads");
 const investment = require("../../models/Investment");
+const User = require("../../models/User");
+const { allusers } = require("./usersSection");
 exports.userDashboard = async (req, res) => {
   let user = req.user.user;
   const userId = user.userId;
@@ -34,15 +36,25 @@ exports.userDashboard = async (req, res) => {
   if (!(income < Number(2) * Number(amount))) {
     income = Number(2) * Number(amount);
   }
-  console.log("income", income);
 
   //===============================================
   let newincome = Number(totalRoI) + Number(levelIncome);
-  console.log("income", newincome);
   if (!(newincome < 2 * Number(amount)))
     newincome = 2 * Number(amount) - Number(withdrawDetail);
   else newincome = Number(newincome) - Number(withdrawDetail);
+  //====================================================
 
+  let allUser = await User.find({});
+  console.log(allUser.length);
+  let allActiveMembers = Number(0);
+  let allDeactiveMembers = Number(0);
+  for (let i = 0; i < allusers.length; i++) {
+    //console.log(allusers[i]);
+    // if (allusers[i].isInvested) allActiveMembers += 1;
+    // else allDeactiveMembers += 1;
+  }
+  console.log("allDeactiveMembers", allDeactiveMembers);
+  console.log("allActiveMembers", allActiveMembers);
   let totalROIAdmin = await totalROIForAdmin();
   let totalLEVELAdmin = await totalLEVELForAdmin();
   totalinvestment = amount;
@@ -57,6 +69,6 @@ exports.userDashboard = async (req, res) => {
   dashboardInfo["totalROIAdmin"] = totalROIAdmin;
   dashboardInfo["totalLEVELAdmin"] = totalLEVELAdmin;
   dashboardInfo["avaibalance"] = newincome;
-
+  dashboardInfo["totalmembersInfoForadmin"] = allUser.length;
   res.status(200).json({ result: dashboardInfo });
 };
